fa6cfa7decf5f3efc4d4bb4e679cb3d85b386ab2
tnemn <tnemnorivnelee@gmail.com>
2024-10-01 23:20:34 +0800

    2장 / 아이템 4까지 정리 수정

---
+++ src/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+//    private Person(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    public static Person create(String name, int age) {
+//        return new Person(name, age);
+//    }
+}
